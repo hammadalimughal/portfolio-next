5a53e037a48fda07e2cfd8ed449eec9d7fdeeeeb
hammadalimughal <hammmad19ali@gmail.com>
2022-10-27 18:58:36 +0500

    checking error

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import '../styles/globals.css'
 import '../styles/fontawesomev6.css'
-import "@fancyapps/ui/dist/fancybox.esm.js";
-import "@fancyapps/ui/dist/fancybox.css";
+// import "@fancyapps/ui/dist/fancybox.esm.js";
+// import "@fancyapps/ui/dist/fancybox.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
